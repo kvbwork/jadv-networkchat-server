9422440dc6cdc88774d24fa1596e2fde6d237ba6
kvbwork <kvbdev@yandex.ru>
2022-10-23 15:37:07 +0500

    refactor ConnectionAcceptor

--- src/main/java/kvbdev/messenger/server/runnable/ConnectionAcceptor.java
+++ src/main/java/kvbdev/messenger/server/runnable/ConnectionAcceptor.java
@@ -4,16 +4,19 @@ import kvbdev.messenger.server.Connection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.Closeable;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.function.Consumer;
 
-public class ConnectionAcceptor implements Runnable {
+public class ConnectionAcceptor implements Runnable, Closeable {
     private static final long DEFAULT_CONNECTION_TIMEOUT = 60_000;
     private static final Logger logger = LoggerFactory.getLogger(ConnectionAcceptor.class);
-    private final ServerSocket serverSocket;
-    private final Consumer<Connection> onConnection;
+
+    protected ServerSocket serverSocket;
+    protected final Consumer<Connection> onConnection;
 
     public ConnectionAcceptor(int port, Consumer<Connection> onConnection) throws IOException {
         this.serverSocket = new ServerSocket(port);
@@ -30,12 +33,19 @@ public class ConnectionAcceptor implements Runnable {
                     logger.debug("new {}", connection);
                     onConnection.accept(connection);
                 } catch (IOException ex) {
-                    logger.error("error connection creating for {}", clientSocket);
+                    logger.error("connection IO error: {} ({})", ex.getMessage(), clientSocket);
                 }
             }
             serverSocket.close();
+        } catch (SocketException e) {
+            logger.warn("{}", e.getMessage());
         } catch (IOException e) {
             logger.error("", e);
         }
+    }
+
+    @Override
+    public void close() throws IOException {
+        serverSocket.close();
     }
 }
