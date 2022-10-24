b819bd53687bfb5bc259e6cd28b73ef731ec16b7
kvbwork <kvbdev@yandex.ru>
2022-10-24 20:49:36 +0500

    update MessengerServer

--- src/main/java/kvbdev/messenger/server/MessengerServer.java
+++ src/main/java/kvbdev/messenger/server/MessengerServer.java
@@ -1,46 +1,52 @@
 package kvbdev.messenger.server;
 
+import kvbdev.messenger.server.command.EchoCommand;
 import kvbdev.messenger.server.command.ExitCommand;
 import kvbdev.messenger.server.command.LoginCommand;
 import kvbdev.messenger.server.command.UsersCommand;
 import kvbdev.messenger.server.impl.ChatHandler;
-import kvbdev.messenger.server.impl.ChatRoomFileLog;
 import kvbdev.messenger.server.impl.CommandHandler;
+import kvbdev.messenger.server.impl.LoggableChatRoom;
 import kvbdev.messenger.server.runnable.ConnectionAcceptor;
 import kvbdev.messenger.server.runnable.ConnectionsWorker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.Closeable;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-public class MessengerServer {
+public class MessengerServer implements Closeable {
     private static final Logger logger = LoggerFactory.getLogger(MessengerServer.class);
+    private static final long DEFAULT_CONNECTION_TIMEOUT = 60_000;
 
-    private final int port;
-    private final ChatRoom chatRoom;
-    private final Collection<Connection> connectionsStorage;
+    protected final int port;
+    protected final ChatRoom chatRoom;
+    protected final Collection<Connection> connectionsStorage;
 
-    private final ConnectionAcceptor connectionAcceptor;
-    private final ConnectionsWorker connectionsWorker;
-    private final ThreadGroup threadGroup;
+    protected final ConnectionAcceptor connectionAcceptor;
+    protected final ConnectionsWorker connectionsWorker;
+    protected final ThreadGroup threadGroup;
 
     public MessengerServer(int port) throws IOException {
         this.port = port;
-        this.chatRoom = new ChatRoomFileLog("Common", "file.log");
+        this.chatRoom = new LoggableChatRoom("Main", "file.log");
         this.connectionsStorage = new ConcurrentLinkedQueue<>();
         this.threadGroup = new ThreadGroup("MessengerServer");
 
         CommandHandler commandHandler = new CommandHandler();
         commandHandler.register(new ExitCommand());
         commandHandler.register(new LoginCommand(chatRoom));
-        commandHandler.register(new UsersCommand(chatRoom));
+        commandHandler.register(new UsersCommand());
+        commandHandler.register(new EchoCommand());
 
         ChatHandler chatHandler = new ChatHandler();
 
         connectionsWorker = new ConnectionsWorker(connectionsStorage, List.of(commandHandler, chatHandler));
+        connectionsWorker.setConnectionTimeout(DEFAULT_CONNECTION_TIMEOUT);
+
         connectionAcceptor = new ConnectionAcceptor(port, connectionsStorage::add);
     }
 
@@ -55,12 +61,20 @@ public class MessengerServer {
         logger.info("MessengerServer running on port: {}", getPort());
     }
 
-    public void stop() {
+    @Override
+    public void close() throws IOException {
         if (threadGroup.activeCount() > 0) {
-            connectionsWorker.clear();
             threadGroup.interrupt();
+            connectionAcceptor.close();
+            cleanConnections();
             chatRoom.dispose();
             logger.info("MessengerServer was stopped");
         }
     }
+
+    protected void cleanConnections() {
+        connectionsStorage.forEach(Connection::close);
+        connectionsStorage.clear();
+    }
+
 }
